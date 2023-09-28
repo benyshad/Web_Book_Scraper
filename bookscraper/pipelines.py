fc7bcead42aea3d8d6b128b18620aa6a4d33a77b
Shadowbot <benyshad@gmail.com>
2023-09-28 12:05:48 -0400

    added gitignore

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -12,11 +12,11 @@ class BookscraperPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
-        # field_names = adapter.field_names()
-        # for field_name in field_names:
-        #     if field_name != 'description':
-        #         value = adapter.get(field_name)
-        #         adapter[field_name] = value[0].strip()
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'stars':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0]
         
         # lowercase_keys = ['category', 'product_type']
         # for lowercase_key in lowercase_keys:
