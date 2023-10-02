033cb6f034540d36bf40a76081edc2c6270918c8
Shadowbot <benyshad@gmail.com>
2023-10-02 02:29:59 -0400

    intergrated scrapops proxy api

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -1,31 +1,45 @@
 import scrapy
 from bookscraper.items import BookscraperItem
+from dotenv import load_dotenv
+from urllib.parse import urlencode
+import os
+load_dotenv()
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["books.toscrape.com", "proxy.scrapeops.io"]
     start_urls = ["https://books.toscrape.com"]
 
     # custom_settings = {
     #     'FEEDS': { 'data.jsonl': { 'format': 'jsonlines',}}
     #     }
 
+    def start_requests(self):
+        yield scrapy.Request(url=self.get_proxy_url(self.start_urls[0]), callback=self.parse)
+
+    def get_proxy_url(self, page_url):
+        api_key = {'api_key' : os.getenv('API_SECRET_KEY')}
+        api_key = urlencode(api_key)
+        book_page_url = f"https://proxy.scrapeops.io/v1/?{api_key}&url={page_url}"
+        return book_page_url
+
     def parse(self, response):
         books = response.css('article.product_pod')
 
         for book in books:
-            # yield{
-            #     'name' : book.css('h3 a::text').get(),
-            #     'price' : book.css('.product_price .price_color::text').get(),
-            #     'url' : book.css('h3 a::attr(href)').get()
-            # }
+            yield{
+                'name' : book.css('h3 a::text').get(),
+                'price' : book.css('.product_price .price_color::text').get(),
+                'url' : book.css('h3 a::attr(href)').get()
+            }
             book_url = book.css('h3 a::attr(href)').get()
             if 'catalogue/' in book_url:
                 book_page_url = 'https://books.toscrape.com/' + book_url
             else:
                 book_page_url = 'https://books.toscrape.com/catalogue/' + book_url
             if book_page_url == 'https://books.toscrape.com/catalogue/libertarianism-for-beginners_982/index.html' or book_page_url == 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html':
-                yield response.follow(book_page_url, self.parse_book_page)
+                yield response.follow(self.get_proxy_url(book_page_url), self.parse_book_page)
+
 
         # next_page = response.css('li.next a::attr(href)').get()
         # if next is not None:
@@ -33,7 +47,7 @@ class BookspiderSpider(scrapy.Spider):
         #         next_page_url = 'https://books.toscrape.com/' + next_page
         #     else:
         #         next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
-        # yield response.follow(next_page_url, callback=self.parse)
+        # yield response.follow(self.get_proxy_url(next_page_url), callback=self.parse)
 
     def parse_book_page(self, response):
         book_page = response.css('div.page_inner')
